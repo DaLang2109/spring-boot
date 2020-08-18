21a6c7240371808ce4a25881c60f06e05cefeaab
zhangyp <784821370@qq.com>
2020-08-19 07:47:03 +0800

    添加两个实体类dao 方法接口.

---
+++ src/main/java/com/zhang/dao/test1/ArticleRepository.java
@@ -0,0 +1,6 @@
+package com.zhang.dao.test1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+}
---
+++ src/main/java/com/zhang/dao/test2/MessgeRepository.java
@@ -0,0 +1,6 @@
+package com.zhang.dao.test2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessgeRepository extends JpaRepository<Messge,Long> {
+}
