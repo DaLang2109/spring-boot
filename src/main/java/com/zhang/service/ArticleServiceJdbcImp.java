e95e7d14c528a683732e7bb1b73962315b49db0e
zhangyp <784821370@qq.com>
2020-08-28 10:53:57 +0800

    SpringDataJPA+Atomikos 固定配置类

--- src/main/java/com/zhang/service/ArticleServiceJdbcImp.java
+++ src/main/java/com/zhang/service/ArticleServiceJdbcImp.java
@@ -1,7 +1,9 @@
 package com.zhang.service;
 
-import com.zhang.dao.test1.ArticleRepository;
 import com.zhang.dao.test1.Article;
+import com.zhang.dao.test1.ArticleRepository;
+import com.zhang.dao.test2.Messge;
+import com.zhang.dao.test2.MessgeRepository;
 import com.zhang.entity.ArticleVO;
 import com.zhang.utill.DozerUtill;
 import org.dozer.Mapper;
@@ -13,41 +15,46 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ArticleServiceJdbcImp implements ArticleService {
+public class ArticleServiceJdbcImp {
 
     @Resource
     ArticleRepository articleRepository;
+    @Resource
+    MessgeRepository messgeRepository ;
 
     @Resource
     private Mapper dozerMapper;
 
-    @Override
-    @Transactional
+    @javax.transaction.Transactional
     public void saveArticle(ArticleVO articleVO) {
         Article article = dozerMapper.map(articleVO, Article.class);
+        Messge messge = Messge.builder().name("返回提示信息登记").content("一级警告!!!!").build();
+
+        messgeRepository.save(messge);
         articleRepository.save(article);
+        
+//        int i=10/0;
     }
 
-
-    @Override
+    
     @Transactional
     public void updateArticle(ArticleVO articleVO) {
         Article article = dozerMapper.map(articleVO, Article.class);
         articleRepository.save(article);
     }
 
-    @Override
+    
     public void delArticle(Long id) {
         articleRepository.deleteById(id);
     }
 
-    @Override
+    
     public ArticleVO queryById(Long id) {
         Optional<Article> article = articleRepository.findById(id);
         return dozerMapper.map(article,ArticleVO.class);
     }
 
-    @Override
+    
     public List<ArticleVO> queryAll() {
         List<Article> articleList = articleRepository.findAll();
         return DozerUtill.mapList(articleList,ArticleVO.class);
